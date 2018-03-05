6326411a57280d556f05ecf09f10b3dd4d47edb7
spencerwoo98 <spencerwoo98@foxmail.com>
2018-03-05 14:52:15 +0800

    Add bing wallpaper support

--- app/src/main/java/com/spencerwoo/android/catinthebox/WeatherActivity.java
+++ app/src/main/java/com/spencerwoo/android/catinthebox/WeatherActivity.java
@@ -1,22 +1,28 @@
 package com.spencerwoo.android.catinthebox;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.spencerwoo.android.catinthebox.gson.Forcast;
 import com.spencerwoo.android.catinthebox.gson.Weather;
 import com.spencerwoo.android.catinthebox.util.HttpUtil;
 import com.spencerwoo.android.catinthebox.util.Utility;
 
 import java.io.IOException;
+
+import javax.microedition.khronos.opengles.GL;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -38,9 +44,20 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView carWashText;
     private TextView sportText;
 
+    private ImageView bingPicImg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_weather);
 
         // Init all layouts/views
@@ -57,6 +74,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText = (TextView) findViewById(R.id.car_wash_text);
         sportText = (TextView) findViewById(R.id.sport_text);
 
+        bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
+
         // Get weather info
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = prefs.getString("weather", null);
@@ -69,6 +88,12 @@ public class WeatherActivity extends AppCompatActivity {
             String weatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(weatherId);
+        }
+        String bingPic = prefs.getString("bing_pic", null);
+        if (bingPic != null) {
+            Glide.with(this).load(bingPic).into(bingPicImg);
+        } else {
+            loadBingPic();
         }
 
     }
@@ -110,6 +135,7 @@ public class WeatherActivity extends AppCompatActivity {
             }
         });
 
+        loadBingPic();
     }
 
     private void showWeatherInfo(Weather weather) {
@@ -152,4 +178,31 @@ public class WeatherActivity extends AppCompatActivity {
         weatherLayout.setVisibility(View.VISIBLE);
 
     }
+
+
+    private void loadBingPic() {
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.
+                        getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic", bingPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bingPicImg);
+                    }
+                });
+            }
+        });
+    }
+
 }
